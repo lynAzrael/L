86387267c2784998f607510d224cb1c22cc099f3
lilinleeli1234 <932323316@qq.com>
2018-11-23 11:02:01 +0800

    skiplist 插入节点 c c source

---
+++ skiplist/skiplist_c/skiplist.c
@@ -0,0 +1,17 @@
+#include "skiplist.h"
+
+int main() { 
+	list* l = CreateList();
+	(void)insertNode(l, 1, 2);
+	(void)insertNode(l, 2, 3);
+	(void)insertNode(l, 3, 5);
+	(void)insertNode(l, 5, 7);
+	(void)insertNode(l, 6, 10);
+	(void)insertNode(l, 9, 2);
+	(void)insertNode(l, 4, 3);
+	(void)insertNode(l, 7, 1);
+
+	showList(l);
+	
+	return 0;
+}
---
+++ skiplist/skiplist_c/skiplist.h
@@ -0,0 +1,105 @@
+#ifndef _SKIP_LIST_H_
+#define _SKIP_LIST_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define bool int
+#define true 1
+#define false 0
+
+#define MAX_LEVEL 16
+#define MAX_VALUE_LENGTH 128
+
+typedef struct nodeStruct node;
+typedef struct skipList list;
+
+struct nodeStruct {
+	int key;
+	int level;
+	char value[128];
+	node* forward[MAX_LEVEL];
+};
+
+struct skipList {
+	int level;
+	node* header;
+};
+
+node* CreateNode(int level, int key ,char value[MAX_VALUE_LENGTH]) {
+	node* n = (node*)malloc(sizeof(node) + level * sizeof(node*));
+	n->key = key;
+	memcpy(n->value, value, MAX_VALUE_LENGTH - 1);
+    // n->value = value;
+	n->level = level;
+
+	for (int i =0 ; i < level  ; i++) {
+		n->forward[i] = NULL;
+	}
+
+	return n;
+}
+
+list* CreateList() {
+	list* sl = (list*)malloc(sizeof(list));
+	sl->level = MAX_LEVEL - 1;
+
+	node* n = (node*)malloc(sizeof(node) + MAX_LEVEL * sizeof(node*));
+	sl->header = n;
+
+	for (int i = 0; i < MAX_LEVEL - 1; i++) {
+		sl->header->forward[i] = NULL;
+	}
+	return sl;
+}
+
+bool insertNode(list* l, int key, int level) {
+	// 根据key值，创建一个node
+	node* n = (node*)malloc(sizeof(node) + level * sizeof(node*));
+	n->key = key;
+	n->level = level;
+
+	for (int i=0; i < level; i ++) {
+		n->forward[i] = NULL;
+	}
+
+	// 如果节点的level超过了链表的最大level， 则返回异常
+	if (n->level > l->level ) {
+		return false;
+	}
+	
+	for (int i = 0; i < n->level; i++) {
+		node* header = l->header;
+		node** current = &(l->header->forward[i]);
+		if (*current == NULL) {
+			*current = n;
+		} else {
+			while (*current != NULL && (*current)->key < n->key) {
+				current = &((*current)->forward[i]);
+			} 
+			if (*current == NULL){
+				*current = n;
+			} else if ((*current)->key > n->key) {
+				n->forward[i] = *current;
+				*current = n;
+			}
+		}
+	}
+	return true;
+}
+
+void showList(list* l){
+	for (int i = MAX_LEVEL - 1; i >= 0 ;i--){
+		printf("In level %d...\n", i);
+		node* n = l->header->forward[i];
+		printf("header");
+		while (n != NULL){
+			printf("\t->\t%d", n->key);
+			n = n->forward[i];
+		}
+		printf("\n");
+	}
+}
+
+#endif
